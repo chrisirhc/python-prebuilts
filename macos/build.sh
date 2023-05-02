098182e2efb2809008cae74f41355210406fb700
chua <chua@uber.com>
2023-05-02 08:04:37 +0000

    update mac python sha

--- macos/build.sh
+++ macos/build.sh
@@ -10,7 +10,7 @@ curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 # Get the relocatable python repo
 git clone https://github.com/gregneagle/relocatable-python.git
 pushd relocatable-python
-git checkout 8bce58e91895978da6f238c1d2e1de3559ea4643
+git checkout 67648ffc91aef264f0f8eb2eba14f9ed126f4168
 git apply ../relocatable-python.patch
 
 for version in "3.9.13_10.9"
